c8b8e2220930a48b58a50fe03ea5b4826220fcec
dzenilee <dzenilee@gmail.com>
2019-12-01 10:51:15 -0800

    rename/move files

--- src/presidential/utils/__init__.py
+++ src/presidential/utils/__init__.py
@@ -1,4 +1,7 @@
 from pathlib import Path
 
-PRESIDENTIAL_DIR = Path(__file__).resolve().parent.parent
-DATA_DIR = PRESIDENTIAL_DIR / "data"
+# PRESIDENTIAL_DIR = Path(__file__).resolve().parent.parent
+# DATA_DIR = PRESIDENTIAL_DIR / "data"
+SOURCE_DIR = Path(__file__).resolve().parent.parent.parent  # .../src/
+ROOT_DIR = SOURCE_DIR.parent
+DATA_DIR = ROOT_DIR / "data"
